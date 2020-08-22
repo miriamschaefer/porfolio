c91951861adf80b627b4c09c8e3e6f42ae43effc
miriamschaefer <msuarezmelian@gmail.com>
2020-08-22 18:33:33 +0200

    set project structure and mainpage

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../stylesheets/App.scss';
+
+function App() {
+  return (
+    <main className="main">
+      <h1 className="main__title">
+        Hey, <span>it's Miriam!</span>
+      </h1>
+    </main>
+  );
+}
+
+export default App;
