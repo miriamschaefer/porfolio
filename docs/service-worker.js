7ea015b9c6a01c1041e7f9ae5dcbb494f31b8627
miriamschaefer <msuarezmelian@gmail.com>
2020-08-23 10:30:58 +0200

    Update docs

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a9731caee950faa3ca26a2fe654e63cc.js"
+  "./precache-manifest.59ae9a4cf8d880cedecfeb5e889b6f3d.js"
 );
 
 self.addEventListener('message', (event) => {
