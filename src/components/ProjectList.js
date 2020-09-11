00a5c7a24995f59575cf842c51a51ae3d760b664
miriamschaefer <msuarezmelian@gmail.com>
2020-09-11 21:26:02 +0200

    set structure for hover over projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,21 @@ import React, { useState } from 'react';
 import Project from './Project';
 
 const ProjectList = (props) => {
+  const [hover, setMouseOver] = useState('true');
+
+  let handleMouseOver = (hover) => {
+    setMouseOver({ hover: true });
+  };
+
+  // let handleMouseOut = (hover) => {
+  //   setMouseOut({ hover: false });
+  // };
+
   const listProject = props.projects.map((project) => {
     return (
       <li key={project.id}>
-        <div className="projects__fondo">
-          <Project project={project} />
+        <div className={`projects__fondo ${hover === true ? '' : 'hidden'}`}>
+          <Project project={project} handleMouseOver={hover} />
         </div>
       </li>
     );
