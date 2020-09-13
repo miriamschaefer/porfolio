cb9a47882963da0ddc5b24cf0e3759e2db26cdec
miriamschaefer <msuarezmelian@gmail.com>
2020-09-13 23:02:52 +0200

    overdoing this image thing...

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,22 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Project = (props) => {
-  const [isShown, setIsShown] = useState(false);
-  const { name, image } = props.project;
+  const { name } = props.project;
   return (
     <>
-      {isShown && (
-        <div className="main__projects__item__img">
-          <img src={image} alt={name} />
-        </div>
-      )}
-      <h2
-        className="main__projects__item__name"
-        onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}
-      >
-        {name}
-      </h2>
+      <h2 className="main__projects__item__name">{name}</h2>
     </>
   );
 };
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Project from './Project';
 
 const ProjectList = (props) => {
-  const { id } = props;
+  const [isShown, setIsShown] = useState(false);
 
   const listProject = props.projects.map((project) => {
     return (
-      <li className="main__projects__item" key={id} id={id}>
+      <li
+        className="main__projects__item"
+        key={project.id}
+        id={project.id}
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      >
         <Project project={project} />
       </li>
     );
   });
 
-  return <ul className="main__projects">{listProject}</ul>;
+  const projectImg = props.projects.map((project) => {
+    return <p>{`${project.id}`}</p>;
+  });
+
+  return (
+    <ul className="main__projects">
+      {listProject}
+      {isShown && `${projectImg}`}
+    </ul>
+  );
 };
 
 export default ProjectList;
