0aaf6d2edf1b564f85be3ca364abf4d0a0660282
miriamschaefer <msuarezmelian@gmail.com>
2020-09-25 13:18:54 +0200

    start setting random fact section

--- src/components/Stuff.js
+++ src/components/Stuff.js
@@ -2,7 +2,82 @@ import React from 'react';
 import heart from '../images/emoji-heart.png';
 import Fade from 'react-reveal/Fade';
 
+// const Stuff = () => {
+//   return (
+//     <Fade left>
+//       <section className="main__stuff">
+//         <h2 className="main__stuff__title">Things that really matter</h2>
+
+//         <ul className="main__stuff__list">
+//           <li className="main__stuff__list__item">
+//             <div className="main__stuff__list__item__icon">
+//               <img src={heart} alt="emoji" />
+//             </div>
+//             <p>I love Oasis and Bad Bunny</p>
+//           </li>
+//           <li className="main__stuff__list__item">
+//             <div className="main__stuff__list__item__icon">
+//               <img src={heart} alt="emoji" />
+//             </div>
+//             <p>I have a dog named Croqueta</p>
+//           </li>
+//           <li className="main__stuff__list__item">
+//             <div className="main__stuff__list__item__icon">
+//               <img src={heart} alt="emoji" />
+//             </div>
+//             <p>
+//               According to this list I keep, I've been to more than 120 concerts
+//               throughout my life
+//             </p>
+//           </li>
+//           <li className="main__stuff__list__item">
+//             <div className="main__stuff__list__item__icon">
+//               <img src={heart} alt="emoji" />
+//             </div>
+//             <p>I have never broken a bone</p>
+//           </li>
+//           <li className="main__stuff__list__item">
+//             <div className="main__stuff__list__item__icon">
+//               <img src={heart} alt="emoji" />
+//             </div>
+//             <p>I kinda miss Myspace</p>
+//           </li>
+
+//           <li className="main__stuff__list__item">
+//             <div className="main__stuff__list__item__icon">
+//               <img src={heart} alt="emoji" />
+//             </div>
+//             <p>
+//               I make reeeeally good{' '}
+//               <a
+//                 href="https://open.spotify.com/user/eme_x?si=wzjkxOwfSI2b30YwTkr2RA"
+//                 alt="Spotify profile"
+//               >
+//                 playlists
+//               </a>
+//             </p>
+//           </li>
+//         </ul>
+//       </section>
+//     </Fade>
+//   );
+// };
+
+const facts = [
+  'I love Oasis and Bad Bunny',
+  'I have a dog named Croqueta',
+  'I make really good playlists',
+];
+
 const Stuff = () => {
+  function getFact() {
+    facts.forEach(function (fact, index) {
+      setTimeout(function () {
+        return console.log(fact);
+      }, 5000 * (index + 1));
+    });
+  }
+
   return (
     <Fade left>
       <section className="main__stuff">
@@ -13,54 +88,11 @@ const Stuff = () => {
             <div className="main__stuff__list__item__icon">
               <img src={heart} alt="emoji" />
             </div>
-            <p>I love Oasis and Bad Bunny</p>
-          </li>
-          <li className="main__stuff__list__item">
-            <div className="main__stuff__list__item__icon">
-              <img src={heart} alt="emoji" />
-            </div>
-            <p>I have a dog named Croqueta</p>
-          </li>
-          <li className="main__stuff__list__item">
-            <div className="main__stuff__list__item__icon">
-              <img src={heart} alt="emoji" />
-            </div>
-            <p>
-              According to this list I keep, I've been to more than 120 concerts
-              throughout my life
-            </p>
-          </li>
-          <li className="main__stuff__list__item">
-            <div className="main__stuff__list__item__icon">
-              <img src={heart} alt="emoji" />
-            </div>
-            <p>I have never broken a bone</p>
-          </li>
-          <li className="main__stuff__list__item">
-            <div className="main__stuff__list__item__icon">
-              <img src={heart} alt="emoji" />
-            </div>
-            <p>I kinda miss Myspace</p>
-          </li>
-
-          <li className="main__stuff__list__item">
-            <div className="main__stuff__list__item__icon">
-              <img src={heart} alt="emoji" />
-            </div>
-            <p>
-              I make reeeeally good{' '}
-              <a
-                href="https://open.spotify.com/user/eme_x?si=wzjkxOwfSI2b30YwTkr2RA"
-                alt="Spotify profile"
-              >
-                playlists
-              </a>
-            </p>
+            {getFact()}
           </li>
         </ul>
       </section>
     </Fade>
   );
 };
-
 export default Stuff;
