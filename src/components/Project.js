cb254cb6958ab9454151eeef22a9586bc9264861
miriamschaefer <msuarezmelian@gmail.com>
2021-01-04 20:12:12 +0100

    show image on hover on project list

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Project = (props) => {
-  const { name, id } = props.project;
+
+  const [isShown, setIsShown] = useState(false);
+
+  const { name, id, image } = props.project;
   return (
     <Link to={`/project/${id}`}>
-      <li className="main__projects__item" key={id} id={id}>
+      <li className="main__projects__item"
+      key={id}
+      id={id}
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}>
         {name}
+        {isShown && (
+          <div className="pruebacentrado">
+        <img src={image} alt={name} />
+        </div>
+        )}
       </li>
     </Link>
   );
